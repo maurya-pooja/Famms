0c2a75f5f45b44c7d71a548a97838ceab811cf9b
maurya-pooja <pooja5570maurya@gmail.com>
2025-05-19 11:42:03 +0530

    first push

---
+++ script.js
@@ -0,0 +1,31 @@
+// alert("hello world");
+// console.log(5+1);
+
+// document.getElementById("demo").innerHTML="pooja";
+// document.getElementById("demo").style.backgroundColor="yellow";
+
+
+function myFunction() {
+    console.log("button Clicked");
+    document.getElementById("Vt-Nav").style.display = "block";
+    document.getElementById("btn-btn").style.display  = "none";
+    document.getElementById("btn-btn-btn").style.display = "block"
+
+
+    // if(style.display = "block")
+  }
+
+  function closebtn() {
+    console.log("close clcik");
+    document.getElementById("Vt-Nav").style.display = "none";
+
+     document.getElementById("btn-btn-btn").style.display ="none"
+
+     document.getElementById("btn-btn").style.display  = "block";
+    
+  }
+
+
+
+
+
